acca6c47885143a344f5447bca3996a7dc1b3d53
Imabirdmf <vash.stampide@gmail.com>
2025-12-18 23:05:16 +0000

    task 4

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,7 @@
 import argparse
 import json
+from itertools import zip_longest
+
 
 # first_file = 'files/file1.json'
 # second_file = 'files/file2.json'
@@ -13,6 +15,7 @@ def main():
     parser.add_argument('second_file')
     parser.add_argument(
         '-f', '--format',
+        default='json',
         help='set format of output'
     )
 
@@ -21,12 +24,30 @@ def main():
     file1_path = args.first_file
     file2_path = args.second_file
 
-    with open(file1_path) as f1, open(file2_path) as f2:
+    generate_diff(file1_path, file2_path)
+    # print(f"Loaded data keys: {list(data1.items())} and {list(data2.items())}")
+    # print(f"Comparing {file1_path} and {file2_path} with format {args.format}")
+
+def generate_diff(file_path1, file_path2):
+    with open(file_path1) as f1, open(file_path2) as f2:
         data1 = json.load(f1)
         data2 = json.load(f2)
 
-    print(f"Loaded data keys: {list(data1.keys())} and {list(data2.keys())}")
-    print(f"Comparing {file1_path} and {file2_path} with format {args.format}")
+
+        result = '{\n'
+        for k in sorted(max(data1, data2, key=len)):
+            if data1.get(k) is None:
+                result += f'+ {k}: {data2[k]}\n'
+            elif data2.get(k) is None:
+                result += f'- {k}: {data1[k]}\n'
+            elif data1.get(k) == data2.get(k):
+                result += f'  {k}: {data1[k]}\n'
+            elif data1.get(k) != data2.get(k):
+                result += f'- {k}: {data1[k]}\n'\
+                      f'+ {k}: {data2[k]}\n'
+        result += '}'
+        print(result)
+
 
 if __name__ == '__main__':
     main()
