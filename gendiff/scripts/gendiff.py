b1d629b9640e4f5f7ef0ebe4c6bf32c8c22dc74c
Imabirdmf <vash.stampide@gmail.com>
2025-12-19 00:17:41 +0000

    task 4. fixes

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,5 @@
 import argparse
-import json
-from itertools import zip_longest
+from gendiff import generate_diff
 
 
 # first_file = 'files/file1.json'
@@ -24,30 +23,11 @@ def main():
     file1_path = args.first_file
     file2_path = args.second_file
 
-    generate_diff(file1_path, file2_path)
     # print(f"Loaded data keys: {list(data1.items())} and {list(data2.items())}")
     # print(f"Comparing {file1_path} and {file2_path} with format {args.format}")
 
-def generate_diff(file_path1, file_path2):
-    with open(file_path1) as f1, open(file_path2) as f2:
-        data1 = json.load(f1)
-        data2 = json.load(f2)
-
-
-        result = '{\n'
-        for k in sorted(max(data1, data2, key=len)):
-            if data1.get(k) is None:
-                result += f'+ {k}: {data2[k]}\n'
-            elif data2.get(k) is None:
-                result += f'- {k}: {data1[k]}\n'
-            elif data1.get(k) == data2.get(k):
-                result += f'  {k}: {data1[k]}\n'
-            elif data1.get(k) != data2.get(k):
-                result += f'- {k}: {data1[k]}\n'\
-                      f'+ {k}: {data2[k]}\n'
-        result += '}'
-        print(result)
-
+    diff = generate_diff(args.first_file, args.second_file)
+    print(diff)
 
 if __name__ == '__main__':
     main()
