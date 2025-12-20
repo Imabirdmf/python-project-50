d0dc1e143004b8c0fcb7de9b9686d4d546c0d96b
Imabirdmf <vash.stampide@gmail.com>
2025-12-20 21:55:49 +0000

    more fixes

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -6,15 +6,16 @@ def generate_diff(file_path1, file_path2):
         data1 = json.load(f1)
         data2 = json.load(f2)
 
-        result = "{\n"
-        for k in sorted(max(data1, data2, key=len)):
-            if data1.get(k) is None:
-                result += f"+ {k}: {data2[k]}\n"
-            elif data2.get(k) is None:
-                result += f"- {k}: {data1[k]}\n"
+        result = ["{"]
+        for k in sorted(data1.keys() | data2.keys()):
+            if k in data2 and k not in data1:
+                result.append(f"+ {k}: {data2.get(k)}")
+            elif k in data1 and k not in data2:
+                result.append(f"- {k}: {data1[k]}")
             elif data1.get(k) == data2.get(k):
-                result += f"  {k}: {data1[k]}\n"
+                result.append(f"  {k}: {data1[k]}")
             elif data1.get(k) != data2.get(k):
-                result += f"- {k}: {data1[k]}\n+ {k}: {data2[k]}\n"
-        result += "}"
-        return result
+                result.append(f"- {k}: {data1[k]}")
+                result.append(f"+ {k}: {data2[k]}")
+        result.append("}")
+        return "\n".join(result)
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,62 +1,83 @@
+import json
+
+import pytest
+
 from gendiff import generate_diff
 
 
-def test_diff_values():
-    result = generate_diff("files/file1.json", "files/file2.json")
-    expected = (
-        "{\n"
-        "- follow: False\n"
-        "  host: hexlet.io\n"
-        "- proxy: 123.234.53.22\n"
-        "- timeout: 50\n"
-        "+ timeout: 20\n}"
-    )
+def test_extra_value_second():
+    result = generate_diff("tests/test_data/1.0.json", "tests/test_data/1.1.json")
+    expected = "{\n" "  a: 1\n" "  b: 1\n" "  c: 1\n" "+ d: 2\n}"
     assert result == expected
 
-def test_all_diff_values_first():
-    result = generate_diff("tests/test_data/empty.json", "tests/test_data/full.json")
-    expected = (
-        "{\n"
-        "+ follow: False\n"
-        "+ host: hexlet.io\n"
-        "+ proxy: 123.234.53.22\n"
-        "+ timeout: 50\n}"
-    )
+
+def test_not_all_keys_in_second():
+    result = generate_diff("tests/test_data/1.0.json", "tests/test_data/1.2.json")
+    expected = "{\n" "  a: 1\n" "  b: 1\n" "- c: 1\n}"
     assert result == expected
 
-def test_all_diff_values_second():
-    result = generate_diff("tests/test_data/full.json", "tests/test_data/empty.json")
-    expected = (
-        "{\n"
-        "- follow: False\n"
-        "- host: hexlet.io\n"
-        "- proxy: 123.234.53.22\n"
-        "- timeout: 50\n}"
-    )
-    assert result == expected
 
 def test_all_diff_values():
-    result = generate_diff("tests/test_data/full.json", "tests/test_data/full_with_diff_values.json")
-    expected = (
-        "{\n"
-        "- follow: False\n"
-        "+ follow: True\n"
-        "- host: hexlet.io\n"
-        "+ host: google.com\n"
-        "- proxy: 123.234.53.22\n"
-        "+ proxy: 125.534.23.52\n"
-        "- timeout: 50\n"
-        "+ timeout: 40\n}"
-    )
+    result = generate_diff("tests/test_data/1.0.json", "tests/test_data/2.0.json")
+    expected = "{\n" "- a: 1\n" "+ a: 2\n" "- b: 1\n" "+ b: 2\n" "- c: 1\n" "+ c: 2\n}"
     assert result == expected
 
+
 def test_all_same_values():
-    result = generate_diff("tests/test_data/full.json", "tests/test_data/full.json")
-    expected = (
-        "{\n"
-        "  follow: False\n"
-        "  host: hexlet.io\n"
-        "  proxy: 123.234.53.22\n"
-        "  timeout: 50\n}"
-    )
+    result = generate_diff("tests/test_data/1.0.json", "tests/test_data/1.0.json")
+    expected = "{\n" "  a: 1\n" "  b: 1\n" "  c: 1\n}"
     assert result == expected
+
+
+def test_empty_files():
+    result = generate_diff("tests/test_data/empty.json", "tests/test_data/empty.json")
+    expected = "{\n}"
+    assert result == expected
+
+
+def test_unique_value_in_second():
+    result = generate_diff("tests/test_data/2.0.json", "tests/test_data/2.1.json")
+    exected = "{\n" "  a: 2\n" "- b: 2\n" "- c: 2\n" "+ d: 2\n}"
+    assert result == exected
+
+
+def test_boolean_values():
+    result = generate_diff("tests/test_data/3.0.json", "tests/test_data/3.1.json")
+    exected = (
+        "{\n"
+        "- a: True\n"
+        "+ a: False\n"
+        "- d: False\n"
+        "+ d: True\n"
+        "- e: False\n"
+        "+ e: True\n}"
+    )
+    assert result == exected
+
+
+def test_mixed_values():
+    result = generate_diff("tests/test_data/4.0.json", "tests/test_data/4.1.json")
+    exected = (
+        "{\n"
+        "- a: True\n"
+        "+ a: False\n"
+        "- b: abcd\n"
+        "+ b: abcdghjk\n"
+        "- c: 1\n"
+        "+ c: 10\n"
+        "- d: ['abc']\n"
+        "+ d: ['abc', 'new']\n"
+        "- e: None\n"
+        "+ f: {'g': 0}\n}"
+    )
+    assert result == exected
+
+
+def test_file_not_found():
+    with pytest.raises(FileNotFoundError):
+        generate_diff("non_existent.json", "tests/test_data/4.0.json")
+
+
+def test_invalid_json():
+    with pytest.raises(json.JSONDecodeError):
+        generate_diff("tests/test_data/invalid.json", "tests/test_data/4.0.json")
