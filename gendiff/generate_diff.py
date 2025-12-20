4daa47431b9c53e45e4bd4d28ca07686aeb09c1d
Imabirdmf <vash.stampide@gmail.com>
2025-12-20 01:03:12 +0000

    lint + fixes

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,21 +1,20 @@
 import json
 
+
 def generate_diff(file_path1, file_path2):
     with open(file_path1) as f1, open(file_path2) as f2:
         data1 = json.load(f1)
         data2 = json.load(f2)
 
-
-        result = '{\n'
+        result = "{\n"
         for k in sorted(max(data1, data2, key=len)):
             if data1.get(k) is None:
-                result += f'+ {k}: {data2[k]}\n'
+                result += f"+ {k}: {data2[k]}\n"
             elif data2.get(k) is None:
-                result += f'- {k}: {data1[k]}\n'
+                result += f"- {k}: {data1[k]}\n"
             elif data1.get(k) == data2.get(k):
-                result += f'  {k}: {data1[k]}\n'
+                result += f"  {k}: {data1[k]}\n"
             elif data1.get(k) != data2.get(k):
-                result += f'- {k}: {data1[k]}\n'\
-                      f'+ {k}: {data2[k]}\n'
-        result += '}'
+                result += f"- {k}: {data1[k]}\n" f"+ {k}: {data2[k]}\n"
+        result += "}"
         return result
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,27 +1,21 @@
 import argparse
-from gendiff import generate_diff
 
+from gendiff import generate_diff
 
 # first_file = 'files/file1.json'
 # second_file = 'files/file2.json'
 
+
 def main():
     parser = argparse.ArgumentParser(
-        description='Compares two configuration files and shows a difference.',
-        prog='gendiff'
+        description="Compares two configuration files and shows a difference.",
+        prog="gendiff",
     )
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    parser.add_argument(
-        '-f', '--format',
-        default='json',
-        help='set format of output'
-    )
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    parser.add_argument("-f", "--format", default="json", help="set format of output")
 
     args = parser.parse_args()
-
-    file1_path = args.first_file
-    file2_path = args.second_file
 
     # print(f"Loaded data keys: {list(data1.items())} and {list(data2.items())}")
     # print(f"Comparing {file1_path} and {file2_path} with format {args.format}")
@@ -29,5 +23,6 @@ def main():
     diff = generate_diff(args.first_file, args.second_file)
     print(diff)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
